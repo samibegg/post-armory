39fc6ccfbf1c08df06d17629b3adc088c5d11b13
samibegg <sami.begg@mongodb.com>
2025-06-24 16:04:53 -0400

    logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
+// /pages/_app.js
+
 import '../styles/globals.css'
 import { SessionProvider } from "next-auth/react"
+import Head from 'next/head'; // 1. Import the Head component
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    // 2. Use a Fragment (<>) to wrap multiple components
+    <>
+      <Head>
+        <title>Post Armory</title> {/* 3. Set your default site title here */}
+        <link rel="icon" href="/logo.png" /> {/* 4. Add the link to your icon */}
+      </Head>
+      
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   )
 }
